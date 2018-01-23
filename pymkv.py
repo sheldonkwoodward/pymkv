81b379c390664dee9686328babdafe93b10efed2
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-01-23 15:16:01 -0800

    Added parameters for tracks. Restructured command generator.

--- pymkv.py
+++ pymkv.py
@@ -1,74 +1,62 @@
 import subprocess as sp
 
 
-# TODO: add default, forced, and language parameters
 class Track:
-    def __init__(self, path, title=None):
+    def __init__(self, path, title=None, language=None, default=None, forced=None):
         self.path = path
         self.title = title
+        self.language = language
+        self.default = default
+        self.forced = forced
 
 
 class VideoTrack(Track):
-    def __init__(self, path, title=None):
-        super().__init__(path, title)
-        self.path = path
-        self.title = title
+    def __init__(self, path, title=None, language=None, default=None, forced=None):
+        super().__init__(path, title, language, default, forced)
 
 
 class AudioTrack(Track):
-    def __init__(self, path, title=None):
-        super().__init__(path, title)
-        self.path = path
-        self.title = title
+    def __init__(self, path, title=None, language=None, default=None, forced=None):
+        super().__init__(path, title, language, default, forced)
 
 
 class SubtitleTrack(Track):
-    def __init__(self, path, title=None):
-        super().__init__(path, title)
-        self.path = path
-        self.title = title
+    def __init__(self, path, title=None, language=None, default=None, forced=None):
+        super().__init__(path, title, language, default, forced)
 
 
 class MKVObject:
     def __init__(self, output_file, title=None):
         # initial values
-        self.command = []
         self.output_file = output_file
         self.title = title
         self.tracks = []
 
-    def _build_command(self):
+    # execution
+    def command(self):
         # setup command
-        self.command = ['mkvmerge', '-o', self.output_file]
+        command = ['mkvmerge', '-o', self.output_file]
 
         # universal attributes
         if self.title:
-            self.command.extend(['--title', self.title])  # set title
+            command.extend(['--title', self.title])  # set title
 
         # add tracks
         for track in self.tracks:
             if track.title:
-                self.command.extend(['--track-name', '0:' + track.title])
-            self.command.append(track.path)
+                command.extend(['--track-name', '0:' + track.title])
+            command.append(track.path)
+
+        return command
 
     def mux(self):
-        self._build_command()
-        print('Running with command:\n"' + " ".join(self.command) + '"')
-        sp.run(self.command)
+        command = self.command()
+        print('Running with command:\n"' + " ".join(command) + '"')
+        sp.run(command)
 
+    # creation
     def add_track(self, track):
         if isinstance(track, Track):
             self.tracks.append(track)
         else:
             print('Not a track')
-
-
-movie = MKVObject('/Users/sheldonwoodward/Movies/test-project/output.mkv', 'MOVIE 1')
-videoTrack = VideoTrack('/Users/sheldonwoodward/Movies/test-project/tracks/video.h264', 'VIDEO 1')
-audioTrack1 = AudioTrack('/Users/sheldonwoodward/Movies/test-project/tracks/audio.aac', 'AUDIO 1')
-audioTrack2 = AudioTrack('/Users/sheldonwoodward/Movies/test-project/tracks/audio.aac', 'AUDIO 2')
-
-movie.add_track(videoTrack)
-movie.add_track(audioTrack1)
-movie.add_track(audioTrack2)
-movie.mux()
