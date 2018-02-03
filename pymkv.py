bf2b6f2be325f1d034d53cbaf9b351faa5f6d723
Sheldon Woodward <sheldon.k.woodward@gmail.com>
2018-02-02 20:11:29 -0800

    Added removing of tracks from files.

--- pymkv.py
+++ pymkv.py
@@ -2,42 +2,38 @@ import subprocess as sp
 import json
 
 
-class Track:
-    def __init__(self, path, default_track=False, enabled_track=True, forced_track=False, language='eng', number=0,
-                 track_name=None):
+class MKVTrack:
+    def __init__(self, path, track_id, default_track=False, forced_track=False, language='eng', track_name=None):
         self.path = path
+        self.info_json = json.loads(sp.check_output(['mkvmerge', '-J', self.path]).decode('utf8'))
         self.default_track = default_track
-        self.enabled_track = enabled_track
         self.forced_track = forced_track
         self.language = language
-        self.number_original = number
+        self.track_id = track_id
         self.track_name = track_name
+        info_json = json.loads(sp.check_output(['mkvmerge', '-J', self.path]).decode('utf8'))
+        self.track_type = info_json['tracks'][track_id]['type']
 
 
-class File:
+class MKVFile:
     def __init__(self, path=None):
         self.path = path
         self.title = None
         self.tracks = []
         if path:
-            self.info_json = json.loads(sp.check_output(['mkvmerge', '-J', self.path]).decode('utf8'))
-            self.info_string = json.dumps(self.info_json, indent=2)
-            if 'title' in self.info_json['container']['properties']:
-                self.title = self.info_json['container']['properties']['title']
+            info_json = json.loads(sp.check_output(['mkvmerge', '-J', self.path]).decode('utf8'))
+            if 'title' in info_json['container']['properties']:
+                self.title = info_json['container']['properties']['title']
 
             # add tracks with info
-            for track in self.info_json['tracks']:
-                new_track = Track(path=self.path)
+            for track in info_json['tracks']:
+                new_track = MKVTrack(path=self.path, track_id=track['id'])
                 if 'default_track' in track['properties']:
                     new_track.default_track = track['properties']['default_track']
-                if 'enabled_track' in track['properties']:
-                    new_track.enabled_track = track['properties']['enabled_track']
                 if 'forced_track' in track['properties']:
                     new_track.forced_track = track['properties']['forced_track']
                 if 'language' in track['properties']:
                     new_track.language = track['properties']['language']
-                if 'number' in track['properties']:
-                    new_track.number_original = track['properties']['number'] - 1
                 if 'track_name' in track['properties']:
                     new_track.track_name = track['properties']['track_name']
                 self.add_track(new_track)
@@ -46,22 +42,36 @@ class File:
         command = ['mkvmerge', '-o', output_file]
         # universal attributes
         if self.title:
-            command.extend(['--title', '"' + self.title + '"'])  # set title
+            command.extend(['--title', self.title])
         # add tracks
-        for track_num, track in enumerate(self.tracks):
+        for track in self.tracks:
+            # flags
             if track.track_name:
-                command.extend(['--track-name', str(track.number_original) + ':"' + track.track_name + '"'])
+                command.extend(['--track-name', str(track.track_id) + ':' + track.track_name])
             if track.language:
-                command.extend(['--language', str(track.number_original) + ':' + track.language])
+                command.extend(['--language', str(track.track_id) + ':' + track.language])
             if not track.default_track:
-                command.extend(['--default-track', str(track.number_original) + ':0'])
+                command.extend(['--default-track', str(track.track_id) + ':0'])
             if track.forced_track:
-                command.extend(['--forced-track', str(track.number_original) + ':1'])
-            try:
-                if self.tracks[track_num + 1].path != track.path:
-                    command.append(track.path)
-            except IndexError:
-                command.append(track.path)
+                command.extend(['--forced-track', str(track.track_id) + ':1'])
+
+            # remove extra tracks
+            if track.track_type != 'video':
+                command.append('-D')
+            else:
+                command.extend(['-d', str(track.track_id)])
+            if track.track_type != 'audio':
+                command.append('-A')
+            else:
+                command.extend(['-a', str(track.track_id)])
+            if track.track_type != 'subtitles':
+                command.append('-S')
+            else:
+                command.extend(['-s', str(track.track_id)])
+
+            # add path
+            command.append(track.path)
+
         if subprocess:
             return command
         return " ".join(command)
@@ -79,14 +89,12 @@ class File:
             print('\n')
             if track.default_track is not None:
                 print('Default Track: ' + str(track.default_track))
-            if track.enabled_track is not None:
-                print('Enabled Track: ' + str(track.enabled_track))
             if track.forced_track is not None:
                 print('Forced Track: ' + str(track.forced_track))
             if track.language is not None:
                 print('Language: ' + track.language)
-            if track.number_original is not None:
-                print('Number: ' + str(track.number_original))
+            if track.track_id is not None:
+                print('ID: ' + str(track.track_id))
             if track.track_name is not None:
                 print('Track Name: ' + track.track_name)
 
@@ -95,3 +103,6 @@ class File:
 
     def add_file(self, file):
         self.tracks = self.tracks + file.tracks
+
+    def remove_track(self, track_num):
+        del self.tracks[track_num]
