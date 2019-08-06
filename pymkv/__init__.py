5a6699a2bced2aa197b9526f6a3c3f0471a09f1c
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-08-05 22:16:41 -0800

    Added __version__ variable in pymkv package.

--- pymkv/__init__.py
+++ pymkv/__init__.py
@@ -1,5 +1,18 @@
+# sheldon woodward
+# august 5, 2019
+
+from pkg_resources import get_distribution, DistributionNotFound
+
 from .MKVAttachment import *
 from .MKVTrack import *
 from .MKVFile import *
 from .Timestamp import *
 from .Verifications import *
+
+
+# set the version number within the package using setuptools-scm
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = None
