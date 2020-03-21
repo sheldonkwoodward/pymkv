65e65dd21ac3f5c58453187025a1b7527c637620
Sheldon Woodward <sheldon.k.woodward@gmail.com>
2020-03-21 10:53:49 -0700

    Feature/docs (#51)
    * Updated module docstrings.
    
    * Updated README.md with roadmap.
    
    * Updated docs index.rst.
    
    * Improved bug-report issue template.

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,10 +1,19 @@
-Home
-====
+pymkv
+=====
 
-Welcome the pymkv documentation!
+Welcome to the pymkv documentation! Here you will find links to the core modules and examples of how to use each.
 
 Modules
 -------
+
+The three primary modules of pymkv are :class:`~pymkv.MKVFile`, :class:`~pymkv.MKVTrack`, and
+:class:`~pymkv.MKVAttachment`. The :class:`~pymkv.MKVFile` class is used to import existing or create new MKV files.
+The :class:`~pymkv.MKVTrack` class is used to add individual tracks to an :class:`~pymkv.MKVFile`. The
+:class:`~pymkv.MKVAttachment` class is used to add attachments to an :class:`~pymkv.MKVFile`.
+
+Each module supports or mimics many of the same operations as mkvmerge but are not necessarily complete. If there is
+functionality that is missing or an error in the docs, please open a new issue `here <https://github
+.com/sheldonkwoodward/pymkv/issues>`_.
 
 .. toctree::
     :maxdepth: 1
--- pymkv/MKVTrack.py
+++ pymkv/MKVTrack.py
@@ -1,7 +1,39 @@
-# sheldon woodward
-# 2/25/2018
+""":class:`~pymkv.MKVTrack` classes are used to represent tracks within an MKV or to be used in an MKV. They can
+represent a video, audio, or subtitle track.
 
-"""MKVTrack Class"""
+Examples
+--------
+Below are some basic examples of how the :class:`~pymkv.MKVTrack` objects can be used.
+
+Create a new :class:`~pymkv.MKVTrack` from a track file. This example takes a standalone track file and uses it in an
+:class:`~pymkv.MKVTrack`.
+
+>>> from pymkv import MKVTrack
+>>> track1 = MKVTrack('path/to/track.h264')
+>>> track1.track_name = 'Some Name'
+>>> track1.language = 'eng'
+
+Create a new :class:`~pymkv.MKVTrack` from an MKV file. This example will take a specific track from an MKV and also
+prevent any global tags from being included if the :class:`~pymkv.MKVTrack` is muxed into an :class:`~pymkv.MKVFile`.
+
+>>> track2 = MKVTrack('path/to/track.aac')
+>>> track2.language = 'eng'
+
+Create a new :class:`~pymkv.MKVTrack` from an MKV file. This example will take a specific track from an MKV and also
+prevent any global tags from being included if the :class:`~pymkv.MKVTrack` is muxed into an :class:`~pymkv.MKVFile`.
+
+>>> track3 = MKVTrack('path/to/MKV.mkv', track_id=1)
+>>> track3.no_global_tags = True
+
+Now all these tracks can be added to an :class:`~pymkv.MKVFile` object and muxed together.
+
+>>> from pymkv import MKVFile
+>>> file = MKVFile()
+>>> file.add_track(track1)
+>>> file.add_track(track2)
+>>> file.add_track(track3)
+>>> file.mux('path/to/output.mkv')
+"""
 
 import json
 from os.path import expanduser, isfile
@@ -12,27 +44,61 @@ from pymkv.ISO639_2 import is_ISO639_2
 
 
 class MKVTrack:
+    """A class that represents a track for an :class:`~pymkv.MKVFile` object.
+
+    :class:`~pymkv.MKVTrack` objects are video, audio, or subtitles. Tracks can be standalone files or a single track
+    within an MKV file, both can be handled by pymkv. An :class:`~pymkv.MKVTrack` object can be added to an
+    :class:`~pymkv.MKVFile` and will be included when the MKV is muxed.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the track file. This can also be an MKV where the `track_id` is the track represented in the MKV.
+    track_id : int, optional
+        The id of the track to be used from the file. `track_id` only needs to be set when importing a track from
+        an MKV. In this case, you can specify `track_id` to indicate which track from the MKV should be used. If not
+        set, it will import the first track. Track 0 is imported by default because mkvmerge sees standalone
+        track files as having one track with track_id set as 0.
+    track_name : str, optional
+        The name that will be given to the track when muxed into a file.
+    language : str, optional
+        The language of the track. It must be an ISO639-2 language code.
+    default_track : bool, optional
+        Determines if the track should be the default track of its type when muxed into an MKV file.
+    forced_track : bool, optional
+        Determines if the track should be a forced track when muxed into an MKV file.
+
+    Attributes
+    ----------
+    mkvmerge_path : str
+        The path where pymkv looks for the mkvmerge executable. pymkv relies on the mkvmerge executable to parse
+        files. By default, it is assumed mkvmerge is in your shell's $PATH variable. If it is not, you need to set
+        *mkvmerge_path* to the executable location.
+    track_name : str
+        The name that will be given to the track when muxed into a file.
+    default_track : bool
+        Determines if the track should be the default track of its type when muxed into an MKV file.
+    forced_track : bool
+        Determines if the track should be a forced track when muxed into an MKV file.
+    no_chapters : bool
+        If chapters exist in the track file, don't include them when this :class:`~pymkv.MKVTrack` object is a track
+        in an :class:`~pymkv.MKVFile` mux operation. This option has no effect on standalone track files, only tracks
+        that are already part of an MKV file.
+    no_global_tags : bool
+        If global tags exist in the track file, don't include them when this :class:`~pymkv.MKVTrack` object is a track
+        in an :class:`~pymkv.MKVFile` mux operation. This option has no effect on standalone track files, only tracks
+        that are already part of an MKV file.
+    no_track_tags : bool
+        If track tags exist in the specified track within the track file, don't include them when this
+        :class:`~pymkv.MKVTrack` object is a track in an :class:`~pymkv.MKVFile` mux operation. This option has no
+        effect on standalone track files, only tracks that are already part of an MKV file.
+    no_attachments : bool
+        If attachments exist in the track file, don't include them when this :class:`~pymkv.MKVTrack` object is a track
+        in an :class:`~pymkv.MKVFile` mux operation. This option has no effect on standalone track files, only tracks
+        that are already part of an MKV file.
+    """
+
     def __init__(self, file_path, track_id=0, track_name=None, language=None, default_track=False, forced_track=False):
-        """A class that represents an MKV track such as video, audio, or subtitles.
-
-        MKVTracks can be added to an MKVFile. MKVTracks can be video, audio, or subtitle tracks. The only required
-        argument is path which gives the path to a track file.
-
-        file_path (str):
-            Path to the track file. This can also be an MKV where the *track_id* is the track represented in the MKV.
-            This is the only required argument.
-        track_id (int, optional):
-            The id of the track to be used in the file. Does not need to be set unless the input file has multiple
-            tracks.
-        track_name (str, optional):
-            The name that will be given to the track when muxed into a file.
-        language (str, optional):
-            The language of the track. It must be an ISO639-2 language code.
-        default_track (bool, optional):
-            Determines if the track should be the default track of its type when muxed into an MKV file.
-        forced_track (bool, optional):
-            Determines if the track should be a forced track when muxed into an MKV file.
-        """
         # track info
         self._track_codec = None
         self._track_type = None
@@ -63,6 +129,16 @@ class MKVTrack:
 
     @property
     def file_path(self):
+        """str: The path to the track or MKV file containing the desired track.
+
+        Setting this property will verify the passed in file is supported by mkvmerge and set the track_id to 0. It
+        is recommended to recreate MKVTracks instead of setting their file path after instantiation.
+
+        Raises
+        ------
+        ValueError
+            Raised if `file_path` is not a supported file type.
+        """
         return self._file_path
 
     @file_path.setter
@@ -75,6 +151,16 @@ class MKVTrack:
 
     @property
     def track_id(self):
+        """int: The ID of the track within the file.
+
+        Setting *track_id* will check that the ID passed in exists in the file. It will then look at the new track
+        and set the codec and track type. Should be left at 0 unless extracting a specific track from an MKV.
+
+        Raises
+        ------
+        IndexError
+            Raised if the passed in index is out of range of the file's tracks.
+        """
         return self._track_id
 
     @track_id.setter
@@ -88,6 +174,16 @@ class MKVTrack:
 
     @property
     def language(self):
+        """str: The language of the track.
+
+        Setting this property will verify that the passed in language is an ISO-639 language code and use it as the
+        language for the track.
+
+        Raises
+        ------
+        ValueError
+            Raised if the passed in language is not an ISO 639-2 language code.
+        """
         return self._language
 
     @language.setter
@@ -99,6 +195,17 @@ class MKVTrack:
 
     @property
     def tags(self):
+        """str: The tags file to include with the track.
+
+        Setting this property will check that the file path passed in exists and set it as the tags file.
+
+        Raises
+        ------
+        FileNotFoundError
+            Raised if the passed in file does not exist or is not a file.
+        TypeError
+            Raises if the passed in file is not of type str.
+        """
         return self._tags
 
     @tags.setter
@@ -112,8 +219,10 @@ class MKVTrack:
 
     @property
     def track_codec(self):
+        """str: The codec of the track such as h264 or AAC."""
         return self._track_codec
 
     @property
     def track_type(self):
+        """str: The type of track such as video or audio."""
         return self._track_type
