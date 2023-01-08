a54946ac5c27649049bb303ed1a9e6c55cd9c93c
GitBib <bnff@redcollar.ru>
2023-01-08 18:13:08 +0300

    Adding type for tracks to MKVFile.

--- pymkv/MKVFile.py
+++ pymkv/MKVFile.py
@@ -91,7 +91,7 @@ class MKVFile:
         self._global_tags_file = None
         self._link_to_previous_file = None
         self._link_to_next_file = None
-        self.tracks = []
+        self.tracks: list[MKVTrack] = []
         self.attachments = []
 
         if not verify_mkvmerge(mkvmerge_path=mkvmerge_path):
